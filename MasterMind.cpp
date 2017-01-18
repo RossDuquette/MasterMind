bad80c0b85c2699712aaced5ef3c1b1f2cf79d64
Ross Duquette <rduquette423@gmail.com>
2017-01-18 15:12:26 -0500

    Initial Commit

---
+++ MasterMind.cpp
@@ -0,0 +1,168 @@
+#include "MasterMind.h"
+
+int main()
+{
+	//Variable Initialization
+	int counter[4];
+	counter[0] = R; counter[1] = G; counter[2] = B; counter[3] = Y; //initial guess, used to iterate through combinations
+	bool solved = false;
+	vector<MM_Guess> prevGuesses; //stores all previous guesses
+	MM_Guess curGuess; //stores info for current guess
+	MM_Guess correctAnswer = ReadColours(); //Get input from the user (the solution), used to verify guesses
+	
+	while (!solved)
+	{
+		curGuess.SetGuess(counter);
+		
+		CheckGuess(curGuess, correctAnswer);
+		prevGuesses.push_back(curGuess);
+		
+		if (curGuess.Correct())
+			solved = true;
+		else
+			solved = !GetNextGuess(counter, prevGuesses);
+	}
+	
+	PrintGuesses(prevGuesses);
+}
+
+MM_Guess ReadColours()
+{
+	char c[4];
+	int colours[4];
+	MM_Guess ret;
+	cout << "Input 4 different colours (R G B Y P W):\n";
+	cin >> c[0] >> c[1] >> c[2] >> c[3];
+	if (c[0] != c[1] && c[0] != c[2] && c[0] != c[3] && c[1] != c[2] && c[1] != c[3] && c[2] != c[3])
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			switch (c[i])
+			{
+				case 'R': colours[i] = R; break;
+				case 'G': colours[i] = G; break;
+				case 'B': colours[i] = B; break;
+				case 'Y': colours[i] = Y; break;
+				case 'P': colours[i] = P; break;
+				case 'W': colours[i] = W; break;
+				default: return ReadColours();
+			}
+		}
+		ret.SetGuess(colours);
+		ret.SetResult(4, 0);
+		return ret;
+	}
+	else
+		return ReadColours();
+}
+	
+void CheckGuess(MM_Guess& current, MM_Guess correct)
+{
+	int nCorrect = 0, nWrong = 0;
+	int *guess = current.GetGuess(), *answer = correct.GetGuess();
+	for (int i = 0; i < 4; i++)
+	{
+		if (guess[i] == answer[i])
+			nCorrect++;
+		else
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				if (j == i)
+					continue;
+				else if (guess[i] == answer[j])
+					nWrong++;
+			}
+		}
+	}
+	current.SetResult(nCorrect, nWrong);
+}
+
+bool GetNextGuess(int* colours, vector<MM_Guess> prevGuesses)
+{
+	bool done = false;
+	while (!done)
+	{
+		//iterate colours
+		colours[3] = (colours[3] + 1) % 6;
+		if (colours[3] == 0) //addition carry over
+		{
+			colours[2] = (colours[2] + 1) % 6;
+			if (colours[2] == 0)
+			{
+				colours[1] = (colours[1] + 1) % 6;
+				if (colours[1] == 0)
+				{
+					colours[0] = (colours[0] + 1) % 6;
+					if (colours[0] == 0) //cycled through all possible combinations
+						return false;
+				}
+			}
+		}
+		
+		//check for duplicate colours
+		if (colours[0] != colours[1] && colours[0] != colours[2] && colours[0] != colours[3] && 
+			colours[1] != colours[2] && colours[1] != colours[3] && colours[2] != colours[3])
+		{
+			//compare with results from previous guesses
+			int correctSpot, wrongSpot, *prevColours, i;
+			for (i = 0; i < prevGuesses.size(); i++)
+			{
+				//Get previous information
+				prevGuesses[i].GetResult(correctSpot, wrongSpot);
+				prevColours = prevGuesses[i].GetGuess();
+				
+				for (int j = 0; j < 4; j++)
+				{
+					if (colours[j] == prevColours[j])
+						correctSpot--;
+					else
+					{
+						for (int k = 0; k < 4; k++)
+						{
+							if (k == j)
+								continue;
+							else if (colours[j] == prevColours[k])
+							{
+								wrongSpot--;
+								break;
+							}
+						}
+					}
+				}
+				if (correctSpot < 0 || wrongSpot < 0)
+					break;
+			}
+			if (i == prevGuesses.size())
+				done = true;
+		}
+	}
+	return true;
+}
+
+void PrintGuesses(vector<MM_Guess> prevGuesses)
+{
+	int* guess, correctSpot, wrongSpot;
+	for (size_t i = 0; i < prevGuesses.size(); i++)
+	{
+		guess = prevGuesses[i].GetGuess();
+		for (int j = 0; j < 4; j++)
+		{
+			switch (guess[j])
+			{
+				case R: cout << "R "; break;
+				case G: cout << "G "; break;
+				case B: cout << "B "; break;
+				case Y: cout << "Y "; break;
+				case P: cout << "P "; break;
+				case W: cout << "W "; break;
+				default: cout << "ERROR "; break;
+			}
+		}
+		prevGuesses[i].GetResult(correctSpot, wrongSpot);
+		cout << "Correct: " << correctSpot << "  Wrong Spot: " << wrongSpot << endl;
+	}
+}
+	
+	
+
---
+++ MasterMind.h
@@ -0,0 +1,73 @@
+#include <vector>
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+typedef enum eColour
+{
+	R,
+	G,
+	B,
+	Y,
+	P,
+	W
+} eColour;
+
+class MM_Guess
+{
+	private:
+		int Colours[4];
+		int nCorrect;
+		int nWrong;
+		
+	public:
+		MM_Guess();
+		void SetGuess(int* colours);
+		void SetResult(int nCorrectSpot, int nWrongSpot);
+		int* GetGuess();
+		void GetResult(int& nCorrectSpot, int& nWrongSpot);
+		bool Correct();	
+};
+
+MM_Guess ReadColours();
+void CheckGuess(MM_Guess& current, MM_Guess correct);
+bool GetNextGuess(int* colours, vector<MM_Guess> prevGuesses);
+void PrintGuesses(vector<MM_Guess> prevGuesses);
+
+MM_Guess::MM_Guess()
+{
+	nCorrect = 0;
+	nWrong = 0;
+	Colours[0] = -1;
+	Colours[1] = -1;
+	Colours[2] = -1;
+	Colours[3] = -1;
+}
+
+void MM_Guess::SetGuess(int* colours)
+{
+	memcpy(Colours, colours, sizeof(int)*4);
+}
+
+void MM_Guess::SetResult(int nCorrectSpot, int nWrongSpot)
+{
+	nCorrect = nCorrectSpot;
+	nWrong = nWrongSpot;
+}
+
+int* MM_Guess::GetGuess()
+{
+	return Colours;
+}
+
+void MM_Guess::GetResult(int& nCorrectSpot, int& nWrongSpot)
+{
+	nCorrectSpot = nCorrect;
+	nWrongSpot = nWrong;
+}
+
+bool MM_Guess::Correct()
+{
+	return (nCorrect == 4);
+}
